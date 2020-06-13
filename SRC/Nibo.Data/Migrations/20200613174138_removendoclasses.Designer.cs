99d189767c57e52947074b04d3269a7f9cdeca64
Italo Nunes Leite <italonleite@gmail.com>
2020-06-13 14:49:27 -0300

    migration removendoclasses

--- SRC/Nibo.Data/Migrations/20200613174138_removendoclasses.Designer.cs
+++ SRC/Nibo.Data/Migrations/20200613174138_removendoclasses.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nibo.Data.Context;
 
 namespace Nibo.Data.Migrations
 {
     [DbContext(typeof(MeuDbContext))]
-    partial class MeuDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200613174138_removendoclasses")]
+    partial class removendoclasses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SRC/Nibo.Data/Migrations/20200613174138_removendoclasses.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nibo.Data.Migrations
+{
+    public partial class removendoclasses : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
